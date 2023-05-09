fa9ee638a4f9c9e192fbf197c7f6408b9582fafd
FranciscoCarvalho2001 <101589943+FranciscoCarvalho2001@users.noreply.github.com>
2023-05-09 12:32:23 +0100

    Criação do projeto

---
+++ DevWeb_Trab_Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevWeb_Trab_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
