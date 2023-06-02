f9a54df0c86e453250875b1a4d78a352448f1069
DiniSilva <101589841+DiniSilva@users.noreply.github.com>
2023-06-02 19:22:17 +0100

    Anotações das classes "Dispositovos" e "Reparacao"

--- DevWeb_Trab_Final/Models/Dispositivos.cs
+++ DevWeb_Trab_Final/Models/Dispositivos.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
 
 namespace DevWeb_Trab_Final.Models
@@ -17,32 +18,39 @@ namespace DevWeb_Trab_Final.Models
         /// <summary>
         /// Tipo de dispositivo
         /// </summary>
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
         public string Tipo { get; set; }
 
         /// <summary>
         /// Data de Registo do dispositivo
         /// </summary>
-        public DateTime DataReg{ get; set; }   
+        [Display(Name = "Data de registo")]
+        public DateTime DataReg{ get; set; }
 
         /// <summary>
         /// Modelo do dispositivo
         /// </summary>
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
         public string Modelo { get; set; }
 
         /// <summary>
         /// Foto do dispositivo
         /// </summary>
+        [Display(Name = "Imagem do Dispositivo")]
         public string Foto { get; set; }
 
         /// <summary>
         /// Descrição do estado do dispositivo
         /// </summary>
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
+        [Display(Name = "Descrição do estado do dispositivo")]
         public string Estado { get; set; }
         //*************************************************
 
         public ICollection<Reparacao> ListaReparacao { get; set; }
 
         [ForeignKey(nameof(Cliente))]
+        [Display(Name = "Cliente")]
         public int ClienteFK { get; set; }
         public Clientes Cliente { get; set; }
 
--- DevWeb_Trab_Final/Models/Reparacao.cs
+++ DevWeb_Trab_Final/Models/Reparacao.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DevWeb_Trab_Final.Models
 {
@@ -10,19 +11,16 @@ namespace DevWeb_Trab_Final.Models
         public int Id { get; set; }
 
         /// <summary>
-        /// Referência da foto da reparação
-        /// </summary>
-        public string Foto { get; set; }
-
-        /// <summary>
         /// Data do inicio da reparação
         /// </summary>
+        [Display(Name = "Data de início da reparação")]
         public DateTime DataInicio { get; set; }
 
         /// <summary>
         /// Data do fim da reparação
         /// </summary>
-        public DateTime DataFim { get; set; }
+        [Display(Name = "Data do fim da reparação")]
+        public DateTime? DataFim { get; set; }
 
         /// <summary>
         /// Custo da reparação
@@ -30,20 +28,38 @@ namespace DevWeb_Trab_Final.Models
         public int Custo { get; set; }
 
         /// <summary>
+        /// Atributo de auxílio à adição do 
+        /// custo de um dispositivo
+        /// </summary>
+        [NotMapped] // esta anotação impede a EF de exportar este atributo para a BD
+        [RegularExpression("[0-9]+(.|,)?[0-9]{0,2}",
+         ErrorMessage = "Só pode escrever algarismos e, se desejar, duas casas decimais no {0}")]
+        [Display(Name = "Preço")]
+        public string CustoAux { get; set; }
+
+        /// <summary>
         /// Observações relativas ao estado da reparação
         /// </summary>
+        [StringLength(255)]
         public string Observacao { get; set; }
+
+        /// <summary>
+        /// Referência da foto da reparação
+        /// </summary>
+        [Display(Name = "Imagem do Dispositivo")]
+        public string Foto { get; set; }
 
         //*************************************************
 
         [ForeignKey(nameof(Dispositivo))]
+        [Display(Name = "Dispositivo")]
         public int DispositivoFK { get; set; }
         public Dispositivos Dispositivo { get; set; }
 
         [ForeignKey(nameof(Funcionarios))]
+        [Display(Name = "Funcionário")]
         public int FuncionariosFK { get; set; }
         public Funcionarios Funcionarios { get; set; } 
-
 
     }
 }
