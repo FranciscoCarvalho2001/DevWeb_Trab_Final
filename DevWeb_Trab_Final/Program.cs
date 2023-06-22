f12c7411b78adef35801d1ff87a95d10b3acc57a
DiniSilva <101589841+DiniSilva@users.noreply.github.com>
2023-06-22 15:51:39 +0100

    Fix de um bug anterior relacionado com o Registo

--- DevWeb_Trab_Final/Program.cs
+++ DevWeb_Trab_Final/Program.cs
@@ -29,8 +29,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
